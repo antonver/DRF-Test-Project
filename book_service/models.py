1edfce380da1f2faf75f5b275bd0fd47f389424d
antonver <anton.verbovyi@gmail.com>
2024-12-18 11:28:43 +0100

    feat(book_service): Add Book model
    Add a Book model to the book_service app.  This model
    includes fields for title, author, cover type, inventory,
    and rental fee.

--- book_service/models.py
+++ book_service/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.IntegerChoices):
+        SOFT = 0
+        HARD = 1
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.IntegerField(choices=Cover)
+    inventory = models.IntegerField(default=0)
+    fee = models.IntegerField()
+
+
