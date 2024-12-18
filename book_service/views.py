9001455c9700dc80fdebb0dcb6aee363c8194d99
antonver <anton.verbovyi@gmail.com>
2024-12-18 14:16:09 +0100

    feat(api): Restrict book CRUD to admins
    Allow anyone to view book details.

--- book_service/views.py
+++ book_service/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
 
 from book_service.models import Book
 from book_service.serializers import BookSerializer
@@ -10,3 +11,9 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [AllowAny()]
+        elif self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAdminUser()]
+        return super().get_permissions()
