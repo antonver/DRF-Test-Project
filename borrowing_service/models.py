752979481696e31674a2d062f7cdc3e0b6003d01
antonver <anton.verbovyi@gmail.com>
2024-12-19 10:34:06 +0100

    Refactor: Improve borrowing model
    Improve date validation and add string representation.  The date
    validation is now clearer and more robust.  A string
    representation of the model is added for improved debugging and
    display purposes.

--- borrowing_service/models.py
+++ borrowing_service/models.py
@@ -5,13 +5,19 @@ class BorrowingService(models.Model):
     borrow_date = models.DateField()
     expected_return = models.DateField()
     actual_return = models.DateField(blank=True, null=True)
-    book = models.ForeignKey('book_service.Book', on_delete=models.CASCADE)
+    book = models.ForeignKey("book_service.Book", on_delete=models.CASCADE)
     user = models.ForeignKey("user.User", on_delete=models.CASCADE)
 
     def clean(self):
-        if self.expected_return < self.borrow_date or self.actual_return < self.borrow_date:
+        if (
+            self.expected_return < self.borrow_date
+            or self.actual_return < self.borrow_date
+        ):
             return False
 
     def save(self, *args, **kwargs):
         self.full_clean(*args, **kwargs)
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.expected_return
