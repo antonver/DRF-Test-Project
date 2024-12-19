04172d28a45bd02769185a5634ae8594eea00c47
antonver <anton.verbovyi@gmail.com>
2024-12-19 10:34:26 +0100

    feat(borrowing-service): Use separate serializers for create and read
    Improve BorrowingServiceViewSet to use different serializers
    for creating and reading borrowing service entries.  This
    allows for more fine-grained control over data validation
    and representation.  The `create` action now uses
    `BorrowingServiceCreateSerializer`, while `list` and
    `detail` actions use `BorrowingServiceReadSerializer`.  Also,
    querysets are now optimized for better performance.

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -2,15 +2,24 @@ from django.shortcuts import render
 from rest_framework import viewsets
 
 from borrowing_service.models import BorrowingService
-from borrowing_service.serializers import BorrowingServiceReadSerializer
+from borrowing_service.serializers import (
+    BorrowingServiceReadSerializer,
+    BorrowingServiceCreateSerializer,
+)
 
 
 # Create your views here.
 class BorrowingServiceViewSet(viewsets.ModelViewSet):
-    queryset = BorrowingService.objects.all().select_related("book_service.Book", "user.User")
+    queryset = BorrowingService.objects.all()
 
     def get_serializer_class(self):
         if self.action in ["list", "detail"]:
             return BorrowingServiceReadSerializer
         else:
-            pass
+            return BorrowingServiceCreateSerializer
+
+    def get_queryset(self):
+        if self.action in ["list", "detail"]:
+            return self.queryset.select_related("book_service.Book", "user.User")
+        else:
+            self.queryset.select_related("book_service.Book")
