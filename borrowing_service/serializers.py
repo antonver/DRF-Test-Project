eedcc9cf1f90a90d0a4e0d00d434eb41c64db03b
antonver <anton.verbovyi@gmail.com>
2024-12-19 10:34:11 +0100

    feat(borrowing): Implement inventory management on borrowing
    Add inventory check and reduction upon book borrowing.  The
    `check_inventory` and `reduce_inventory` functions ensure that
    a book's inventory is sufficient before creating a new
    borrowing record.  The `user` field is added to the
    `BorrowingServiceReadSerializer` for better data visibility.

--- borrowing_service/serializers.py
+++ borrowing_service/serializers.py
@@ -1,12 +1,37 @@
 from rest_framework import serializers
 
+from book_service.models import Book
 from book_service.serializers import BookSerializer
 from borrowing_service.models import BorrowingService
 
 
 class BorrowingServiceReadSerializer(serializers.ModelSerializer):
     book = BookSerializer
+    user = serializers.CharField(source="user.email")
 
     class Meta:
         model = BorrowingService
-        fields = '__all__'
+        fields = "__all__"
+
+
+def check_inventory(pk: int) -> bool:
+    return Book.objects.get(pk=pk).inventory > 0
+
+
+def reduce_inventory(pk: int) -> None:
+    Book.objects.get(pk=pk).inventory -= 1
+
+
+class BorrowingServiceCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = BorrowingService
+        fields = ["borrow_date", "expected_return", "actual_return", "book"]
+
+    def create(self, **validated_data):
+        book_id = validated_data["book"]
+        if check_inventory(book_id):
+            reduce_inventory(book_id)
+            BorrowingService.objects.create(
+                **validated_data, user=self.context["request"].user
+            )
