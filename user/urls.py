dbf91e50ca0f0a9cc54c6d56692e854499727f1e
antonver <anton.verbovyi@gmail.com>
2024-12-21 19:30:33 +0100

    refactor(user): Remove login URL
    Removed the login URL from the user URLs.  This URL is no
    longer needed in this part of the application.

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,5 @@ app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
