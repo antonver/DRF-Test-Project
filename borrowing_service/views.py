216242d7e58030f6a326468aeeb5365baf286713
antonver <anton.verbovyi@gmail.com>
2024-12-21 19:30:19 +0100

    feat(borrowing_service): Enhance borrowing service views
    Add authentication to borrowing service creation, update, and
    partial update actions.  Restrict data access based on user role
    and add filtering options for active borrowings and user IDs.

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from borrowing_service.models import BorrowingService
 from borrowing_service.serializers import (
@@ -18,8 +19,30 @@ class BorrowingServiceViewSet(viewsets.ModelViewSet):
         else:
             return BorrowingServiceCreateSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
     def get_queryset(self):
+        print(self.request.user.id)
+        queryset = self.queryset
+        if not self.request.user.is_superuser:
+            queryset = queryset.filter(user=self.request.user.id)
         if self.action in ["list", "detail"]:
-            return self.queryset.select_related("book_service.Book", "user.User")
+            queryset = queryset.select_related("book", "user")
         else:
-            self.queryset.select_related("book_service.Book")
+            queryset = queryset.select_related("book")
+        if "is_active" in self.request.query_params:
+            queryset = queryset.filter(actual_return__isnull=True)
+        if (
+            self.request.query_params.get("user_id") is not None
+            and self.request.user.is_superuser
+        ):
+            queryset = queryset.filter(
+                user__id=self.request.query_params.get("user_id")
+            )
+        return queryset
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated()]
+        return super().get_permissions()
