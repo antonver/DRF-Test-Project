bd0d6a6b355562d72b49fbce8f41c9f8da90c526
antonver <anton.verbovyi@gmail.com>
2024-12-21 19:30:06 +0100

    Refactor: Improve borrowing model
    Added indexes to the Borrowing model for better query performance.
    Simplified the `clean` method for improved readability and
    correctness.  The `save` method now calls `full_clean` without
    additional arguments.  The `__str__` method now returns a more
    useful representation of the object.

--- borrowing_service/models.py
+++ borrowing_service/models.py
@@ -8,16 +8,21 @@ class BorrowingService(models.Model):
     book = models.ForeignKey("book_service.Book", on_delete=models.CASCADE)
     user = models.ForeignKey("user.User", on_delete=models.CASCADE)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=["user"]),
+            models.Index(fields=["book"]),
+        ]
+
     def clean(self):
-        if (
-            self.expected_return < self.borrow_date
-            or self.actual_return < self.borrow_date
-        ):
+        if self.expected_return < self.borrow_date:
+            return False
+        if self.actual_return is not None and self.actual_return < self.borrow_date:
             return False
 
     def save(self, *args, **kwargs):
-        self.full_clean(*args, **kwargs)
+        self.full_clean()
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.expected_return
+        return f"{self.expected_return}"
