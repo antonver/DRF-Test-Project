d3a6e727db102eb5430f4a06f099f6de8edd0ae7
antonver <anton.verbovyi@gmail.com>
2024-12-21 19:30:52 +0100

    feat(book_service): Separate serializers for reading and creating books
    This commit separates the serializer for reading book data from
    the serializer used for creating books.  The `BookReadSerializer`
    now includes a computed field for the book cover, improving data
    presentation.

--- book_service/serializers.py
+++ book_service/serializers.py
@@ -3,7 +3,15 @@ from rest_framework import serializers
 from book_service import models
 
 
-class BookSerializer(serializers.ModelSerializer):
+class BookReadSerializer(serializers.ModelSerializer):
+    cover = serializers.CharField(source="get_cover_display", read_only=True)
+
     class Meta:
         model = models.Book
-        fields = '__all__'
+        fields = "__all__"
+
+
+class BookCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Book
+        fields = "__all__"
