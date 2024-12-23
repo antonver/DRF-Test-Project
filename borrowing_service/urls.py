c3a3ad8fbe95282ccbc9cacf96497031b72dfa81
antonver <anton.verbovyi@gmail.com>
2024-12-23 19:21:58 +0100

    feat(api): Add endpoint for returning borrowed items
    Add a new URL endpoint to handle the return date of
    borrowed items.  This allows the system to track
    when items are returned.

--- borrowing_service/urls.py
+++ borrowing_service/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from borrowing_service.views import BorrowingServiceViewSet
+from borrowing_service.views import BorrowingServiceViewSet, return_date
 
 router = DefaultRouter()
 router.register(r"borrow", BorrowingServiceViewSet)
 
-urlpatterns = []
+urlpatterns = [
+    path("return/", return_date, name="return-date"),
+]
 urlpatterns += router.urls
 
 app_name = "borrowing_service"
